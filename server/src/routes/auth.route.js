966d76105d5d67846fd4ab59a445ce6e2fc97cca
shehabrakib <shehabprogram128@gmail.com>
2025-11-17 11:15:25 +0600

    initial commit

---
+++ server/src/routes/auth.route.js
@@ -0,0 +1,15 @@
+import express from "express";
+
+const router = express.Router();
+
+router.post("/signUp", (req, res) => {
+
+});
+router.post("/login", (req, res) => {
+    
+});
+router.get("/logout", (req, res) => {
+    
+});
+
+export default router;
