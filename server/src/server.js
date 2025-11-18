495e5fe8bca7bbca8c9f9641b2fdcd16bac6412e
shehabrakib <shehabprogram128@gmail.com>
2025-11-18 10:33:00 +0600

    ready for deployment

--- server/src/server.js
+++ server/src/server.js
@@ -2,8 +2,10 @@ import express from "express";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
+import path from "path";
 
 dotenv.config();
+const __dirname = path.resolve();
 
 const app = express();
 
@@ -11,6 +13,15 @@ const PORT = process.env.PORT || 3000;
 
 app.use("api/auth", authRoutes);
 app.use("api/message", messageRoutes);
+
+// make ready for deployment replace development with production 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/dist")));
+
+  app.get("*", (_, res) => {
+    res.sendFile(path.join(__dirname, "../client", "dist", "index.html"));
+  });
+}
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
