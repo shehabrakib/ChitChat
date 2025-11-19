d4e39ac1feda7d59f2634496addcfe1dc7c810ab
shehabrakib <shehabprogram128@gmail.com>
2025-11-19 12:09:00 +0600

    login and logout function added

--- server/src/routes/auth.route.js
+++ server/src/routes/auth.route.js
@@ -1,16 +1,12 @@
 import express from "express";
-import { signUp } from "../controllers/auth.controller.js";
+import { login, logout, signUp } from "../controllers/auth.controller.js";
 
 const router = express.Router();
 
 router.post("/signup", signUp);
 
 
-router.post("/login", (req, res) => {
-    
-});
-router.get("/logout", (req, res) => {
-    
-});
+router.post("/login", login);
+router.post("/logout", logout);
 
 export default router;
